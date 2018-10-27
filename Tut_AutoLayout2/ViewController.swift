4dc56fec4f5527408ef2ae69129d446799538669
Eray Alparslan <erayalparslan@yahoo.com>
2018-10-27 14:54:59 +0300

    iOS Notları

---
+++ Tut_AutoLayout2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tut_AutoLayout2
+//
+//  Created by ruroot on 10/27/18.
+//  Copyright © 2018 Eray Alparslan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
